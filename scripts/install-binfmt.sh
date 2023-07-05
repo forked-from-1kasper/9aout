984bc2918630d765cb4c419332a83205dd737249
Siegmentation Fault <siegmentationfault@yandex.ru>
2023-07-05 19:17:28 +0700

    fix

--- scripts/install-binfmt.sh
+++ scripts/install-binfmt.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 if ! [ -e /proc/sys/fs/binfmt_misc/plan9 ]; then
-    echo ":plan9:M::\x00\x00\x8a\x97::$(realpath 9aout):P" > /proc/sys/fs/binfmt_misc/register
+    echo ":plan9:M::\x00\x00\x8a\x97::$(realpath 9aout):" > /proc/sys/fs/binfmt_misc/register
 fi
